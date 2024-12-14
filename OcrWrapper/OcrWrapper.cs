78e806a566514db64e58906b0f2261f1b4b41fc9
datamoonai <hoosh.dade.mahtab@gmail.com>
2024-12-14 14:28:32 +0330

    enabling multi threading

--- OcrWrapper/OcrWrapper.cs
+++ OcrWrapper/OcrWrapper.cs
@@ -5,6 +5,8 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Scripting.Runtime;
 using Python.Runtime;
 
@@ -12,19 +14,23 @@ public class OcrWrapper
 {
     private dynamic _finder;
     private dynamic _np;
+
+    // Set the environment variables
+    string pythonHome = @"OCREngine\Runtime"; // Change this to your Python installation path
+    string pythonDLL = @"OCREngine\Runtime\python310.dll"; // Adjust based on your version
+    string customLibPath = @"OCREngine\Runtime\Lib"; // Change this to your actual path
+
+    bool IsBusy = false;
     public OcrWrapper()
     {
-        // Set the environment variables
-        string pythonHome = @"OCREngine\Runtime"; // Change this to your Python installation path
-        string pythonDLL = @"OCREngine\Runtime\python310.dll"; // Adjust based on your version
-        string customLibPath = @"OCREngine\Runtime\Lib"; // Change this to your actual path
-
+        
         Environment.SetEnvironmentVariable("PYTHONHOME", pythonHome);
         Environment.SetEnvironmentVariable("PYTHONPATH", pythonHome + @"\Lib\site-packages");
 
         // Initialize the Python engine
         Runtime.PythonDLL = pythonDLL; // Specify the Python DLL
         PythonEngine.Initialize();
+        
 
         using (Py.GIL())
         {
@@ -34,6 +40,9 @@ public class OcrWrapper
             _finder = Py.Import("finder");
             _np = Py.Import("numpy");
         }
+
+        Py.GILState state = Py.GIL();
+
     }
 
     public PyObject GetLicenseKey()
@@ -88,6 +97,70 @@ public class OcrWrapper
             return result;
         }
 
+    }
+
+    public void RunTest()
+    {
+        string imagePath = @"europe.png"; // Change this to your JPG file path
+        //using (Py.GIL())
+        //{                       //File.Copy(imagePath, Path.Combine("tests", imagePath.Replace(".jpg", i + ".jpg")),true);
+                                // با استفاده از این تابع نتیجه را دریافت کنیم
+            for (int i = 0; i < 100; i++)
+            {
+
+            if (!IsBusy)
+                Detect(imagePath);
+            else
+                Console.WriteLine("Is Busy!");
+
+
+                Thread.Sleep(50);
+            }
+        //}
+    }
+
+    void Detect(string imagePath)
+    {
+        IsBusy = true;
+        Task.Run(() =>
+        {
+
+
+            var state = PythonEngine.BeginAllowThreads();
+            
+            
+            //OcrWrapper ocrWrapper2 = new OcrWrapper(pythonHome, pythonDLL, customLibPath);
+            using (Bitmap bitmap = new Bitmap(imagePath))
+                {
+                    int width = bitmap.Width;
+                    int height = bitmap.Height;
+                    // Lock the bits of the bitmap
+                    Rectangle rect = new Rectangle(0, 0, width, height);
+                    BitmapData bmpData = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+
+                    // Get the stride of the bitmap
+                    int stride = bmpData.Stride;
+
+
+
+
+                    // Get the address of the first pixel
+                    IntPtr ptr = bmpData.Scan0;
+
+
+
+                    PyObject result = _finder.ocr_bgr(width, height, stride, ptr.ToInt64());
+
+                    Console.WriteLine(result);
+
+                    bitmap.UnlockBits(bmpData);
+
+                }
+            
+            PythonEngine.EndAllowThreads(state);
+            IsBusy = false;
+        });
     }
 }
 
--- Test/Program.cs
+++ Test/Program.cs
@@ -15,24 +15,14 @@ class Program
         Console.WriteLine("Key " + ocrWrapper.GetLicenseKey());
 
         // سپس مجوز را دریافت کنیم و با این تابع ست کنیم - البته این قسمت را یک مرتبه بیشتر نیاز نیست که انجام دهیم
-        Console.WriteLine(ocrWrapper.SetLicenseKey("gAAAAABnMEEBorSvRCZRLhDgDAErJPfzoIbv5N1NEmOHJoYcPDZuQI6QfB5KOsT9S3rKzd1rKw-pTxt09AldXz0TBTepg8MnzqyWrOaO2THhQ4FAvrc88mMzZekcTbmKRCofgDzkbY3-q8_NX-cnG17pRjmK00T--wJYmls1KfieSGWEaqmWXY5Ai860I8GTIOj7yEDNmTyF_UxVcnQRzjtRNhyEzslbrEn2eD0DGnp9n6zkU6sXc1HK2ti5zoWjVZuwgdmP4yRXzdBomqHd109FSN9YNMLWQRtOYfIc0_Z0VN8zbYhpnBcuu_dk4fy5qfgRTvABXduL1sLwzR41HnfcJOgOAKZ6E0Cc_VRtTLzqq_P6oRDk1lDq8NLjo6IMLs0mwwV4rI2pIJc7k7KP8wZhQmTRdICnBgQub8_otcZwlKYE66zg_TfWa98dlIhC_v3Asr7Nzs79YQtmHeJZGjAB_KQxXq9EuUOrTi_cvH9_8WINT1jHZKwXXIBUE4YonQVXd9WYSPpE5dEdg_wYb9R8R7UlXNwMi2Q5Xc-PEAxGN46LjJiN55ymkGAqLM2Xkv8PtTmo92bDznL4KYl2TFI8LZtcTrlvfJji3OwVcNCS6qvv2Ptk3GNgagFNAuD0rOZdgBs9pGkmavGx77oulwvtite4nva3TLo8axS_4vZbJFM-OPCIJWdjemYv-2f5CyKReOZhK7ZBBIq9U9qypwNoCd6J7VPh5gz6YBi2TaPZ4AIqhBsGZk-E-9-ao0R-mtdPWC8qakcv1eYkRun0mx_8kU1lZCvfCNWWF-HKHTvrw888JaCN87iMDRG1wC6rp2vp_DDEfi2V2J_WI7eZK7nqVqLn-5gsr9h17OL1n4_nQUtrS7vdMYM5ra9b_xPa74uqiJa7-W54ZRpdCD7_mI1dF3zicghutMH6-znK3NH4z7MXF6EmHC9C0lQWO_-uBW73qetrgpCqgoNwFjdDYxl9LoKn3mQi06D75pp7qRCIEI9Ru-uSIAU7rUNO8lcgxAuI26I3B60Vi0caOQmPAwVbk0Ua_cNd5_wxbteJmG-b1X-x5D95fdvsCVS55DLkY3e5T8Xy71k6pcd5OVlQEQJN8OJEx7Z693yTl0nIEropkNXQvvpUfVdo1-t00HVyj3XjD6ERl6xnjA3ipZC3VQB7nTQBDcYionjYzx3xo4FL1YEMZpvZM-VxLgcFTn1hT39I5FVbdbdkvI9bhxx4c9mCvFAy6w9OEqzUaRQ-OyaKzW0xIU60mD2r0WCE9vg9mFzgEV7LNReg5Hbzki5wgbz9ZJdLK2j7jQqH-_5t3VGHz23tGW1_noPk_mE-f9gFaDPhUUFbC5A_GHNr0JajqKxOg_AeHmnR2xzSKW6B_wRQsvFpn6dBm3UDG-hnCKEIbOX9kAwgqPJSRPAVR-JFngMvq-hyVsY4h9cU4ZnKg--714NdJ1hs_J_qT4ukyMmjdFdF52LYd_61fDDqwnigG4h0dP2k1n7cqhO5TrGgHgS2Ew7Kfe2ZYd-3woqKSl2hErXycZjZAkdrXeeHD6n4ilx7amJN9tKh7__Ub-JTVGhG2tkxQ8kMhIiiS8vkEurQ1-5hrlQnL697f-pdNwASobQ47aOfjaEBY9bJwpPuifqVJaph8qSjJcM="));
-        //OcrWrapper ocrWrapper2 = new OcrWrapper(pythonHome, pythonDLL, customLibPath);
+        Console.WriteLine(ocrWrapper.SetLicenseKey("gAAAAABnVDGtHdLoliJjb4HWVDscVmKS-l1RtFVTzB7iiabk9n-nWMLKae7wPevd78U6LZykSOye90j7OqlPKEjk99k5kwYdWuESR02A3qsjWuTamzGSp8-vzCDcmm6UTR_LZ6eNtJvx_Q25hqsW57bhqz2ZXps7TB1QX-8luAFP0fMM9mYoc9nXOQwnH6O7muXuwBgCD6JMS_lnnyLiqMPkk5UxABei0he-8pGmG8doefq0W3SpOHQFwXHcTn7quBz7ZTPQYmLs808SY0nfhTdEVvCIQJARHmCTuUzuqLSFRAjmVa6yarTEk6DHV3oTgOxEAbFzaKeh7pNn6WGaqpU-uj1q2qsIweuyuuSmld9pr-PX24Jwn48gbCIScmD4Thm5lfcZ_k3VlCNwY7bkZ0InKEN-rlb7woWH9g1ssYSa43dP3x5Up0XdYs3uhnNwX-Rop-qqUgScVo7OVtpCH6ZDgfEPTD-VfZ_UfyEpBr5ngCbUfjm5iwwwc8J2z_qmHkZpW3ndJG6RPusRj51qDMcZp9dVWULGKUDnsVEH79J3w9q17GsXL1gK_bMQ1Pl5uWSGPrVEuDvN_KilNtOLU-1AtEJTXeiaY8hHckzKfHAn4dp1kyI_sxEEiIEgGllORETu4J1jcjE-uKGdN1AjbDaFdHbKW-j7KCfyn73hKwzgeEOmWhMw8whPDcvCX6hUOFxnNSEdqEvB3M4ovxBQIoKom7QBazpeM_Eohrb9AR7Wh1cL0QCNou0x3Z_euv1lQsxPpLj1wJ2XkZxeFt440PuVB8T6XOQ8o-fYyEHnHy2pyTCRGjhOBTY3-QYp_C_ig8w13g2FirKaHHRRNA5Ivlh8feL6N5e-7_p3mQoq1KXdk04cPGg7KNI4Uuwq5s6LTV4ZlMAfabsVR3eoG6jIDbMt_Jep9k0gNQ2RbyQXFDig7HPIwKKO6Fn78HWZu1IS8xDknmzG52pBNcOXM8g_sL0JExQmos0_ZjeZnrA6Z1Y9eG2oI5mL_kn10-A8Yo0wXVTHPz50lv2-HHYOh3YsvZCxGSDihu6jbuSpwXA-7za8zATJCJ0afuftVlRac5RCTpEMql5oXmCEDpu4PDB-0WAI83mEkwpIiSRTlu1N6nB6mp_ZAP5KCVwTh97IosScW_3DptRB2HBL5r_CYqicHnwtvM6z0hXEo_4R2KiupNSPiQ1CrfV1wFm85UxIYU8Rg7iTNMRzgYqWQA2zPThy5PGqJrERphPvX1x7uTKTAEJfS0ZEFKI4kID90f8wPywZPR6IH4VfGmvqztIRuarw1vVmKWetnp2cfrsNBW9JBw6inWajJkPthsKLdbSmvDkaGS-f90UHJWaXtb__fyxyk8XJiIM3MT5ToU_Ki_vOAlzW00yY5sNeJP1_DAOfziIDQFUAONGNQg8Kh4eJBFTNe2IdL7YdqL5bv0xvKBmXPU4ZqmqwcvI5ocT2GcWgf5wLdRLLmtkVT3Qi1S0Ucwct8uTUutJbLgnG8hrVLorToptktYqddpRv-hH0fwrJLjeBP_N308KCSYN7v1UmI4xd-UCZgiwwGAGeodeZ0tSrFu14FZsIkvg9YXqqQGtipphGUQBi9EmnGywuOkLtjes7wBpbZ0f_h0DAidxj2HAPFlRg9n2gfcG5ib1RcPkVDWaSA3x2_uOa_GCHAw1ldQhddmNTWgU6CRMg-Oms7DbnIzcuhOD-Ozzgm5Vs5EcZ5uEB4MTV380D614yC3neSCO66Rukeg7uSC7Jgg60aWogtONwNnAyVEhGiTrUZp0BFjOSIAEV6p5NTdLyDJ1E694-3ue0c1SDLT7Obw=="));
 
-        // با استفاده از این تابع نتیجه را دریافت کنیم
-        for (int i = 0; i < 100; i++)
-        {
-            string imagePath = @"europe.png"; // Change this to your JPG file path
-            //File.Copy(imagePath, Path.Combine("tests", imagePath.Replace(".jpg", i + ".jpg")),true);
-            using (Bitmap bitmap = new Bitmap(imagePath))
-            {
-
-                Console.WriteLine(ocrWrapper.OCR(bitmap));
-            }
-
-        }
+        ocrWrapper.RunTest();
 
         Console.ReadKey();
     }
+
+
 
 
    
